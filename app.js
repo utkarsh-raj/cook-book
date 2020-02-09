71a36b65e006cafd2dee98bf26b52ca1553b9186
Utkarsh Raj Singh <u.rajsingh2503@gmail.com>
2020-02-10 00:10:05 +0530

    Add describe pages and searching

--- app.js
+++ app.js
@@ -29,12 +29,13 @@ var recepieSchema = new mongoose.Schema({
     cook: { type: String },
     description: { type: String },
     steps: { type: String },
-    image: { type: String }
+    image: { type: String },
+    ingredients: { type: Array }
 });
 
 var ingredientSchema = new mongoose.Schema({
     name: { type: String, default: "New Ingredient" },
-    recepies: { type: Array }
+    recepie: { type: String }
 });
 
 var Cook = mongoose.model("Cook", cookSchema);
@@ -150,6 +151,247 @@ app.get("/landing/:userId", function (req, res) {
         }
     });
 });
+
+// Index
+
+app.get("/index/:userId", function (req, res) {
+    var userId = req.params.userId;
+    Cook.find({
+        _id: userId
+    }, function (err, user) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            Recepie.find({}, function (err, recepies) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log(recepies);
+                    res.render("index", {user: user[0], recepies: recepies});
+                }
+            });
+        }
+    });
+});
+
+// New
+
+app.get("/new/:userId", function (req, res) {
+    var userId = req.params.userId;
+    Cook.find({
+        _id: userId
+    }, function (err, user) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render("new", { user: user[0] });
+        }
+    });
+});
+
+// Edit
+
+app.get("/edit/:userId/:recepieId", function (req, res) {
+    var userId = req.params.userId;
+    var recepieId = req.params.recepieId;
+    Cook.find({
+        _id: userId
+    }, function (err, user) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            Recepie.find({
+                _id: recepieId
+            }, function(err, recepie) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    res.render("edit", { user: user[0], recepie: recepie[0] });
+                }
+            });
+            // res.render("edit", { user: user[0], recepieId: recepieId });
+        }
+    });
+});
+
+app.post("/new/:userId", function (req, res) {
+    var userId = req.params.userId;
+    var name = req.body.name;
+    var description = req.body.description;
+    var steps = req.body.steps;
+    var image = req.body.image;
+    var ingredientsList = [
+        req.body.ingredient1,
+        req.body.ingredient2,
+        req.body.ingredient3,
+        req.body.ingredient4
+    ];
+
+    console.log(userId, name, description, steps, image, ingredientsList);
+
+    // console.log(question);
+
+    Cook.find({
+        _id: userId
+    }, function (err, user) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            Recepie.create({
+                name: name,
+                cook: user[0].username,
+                description: description,
+                steps: steps,
+                image: image,
+                ingredients: ingredientsList
+            }, function (err, recepie) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log(recepie);                    
+                    res.redirect(302, "/index/" + user[0]._id);
+                }
+            });
+        }
+    });
+});
+
+app.post("/edit/:userId/:recepieId", function (req, res) {
+    var userId = req.params.userId;
+    var recepieId = req.params.recepieId;
+    var name = req.body.name;
+    var description = req.body.description;
+    var steps = req.body.steps;
+    var image = req.body.image;
+    var ingredientsList = [
+        req.body.ingredient1,
+        req.body.ingredient2,
+        req.body.ingredient3,
+        req.body.ingredient4
+    ];
+
+    console.log(userId, name, description, steps, image, ingredientsList);
+
+    // console.log(question);
+
+    Cook.find({
+        _id: userId
+    }, function (err, user) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            Recepie.findByIdAndUpdate({_id: recepieId}, {
+                name: name,
+                cook: user[0].username,
+                description: description,
+                steps: steps,
+                image: image,
+                ingredients: ingredientsList
+            }, function (err, recepie) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log(recepie);                    
+                    res.redirect(302, "/index/" + user[0]._id);
+                }
+            });
+        }
+    });
+});
+
+// Detail
+
+app.get("/details/:userId/:recepieId", function(req, res) {
+    Recepie.find({
+        _id: req.params.recepieId
+    }, function(err, recepie) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render("details", {userId: req.params.userId, recepie: recepie[0]});
+        }
+    });
+});
+
+// Search
+
+app.post("/search/:userId", function(req, res) {
+    var userId = req.params.userId;
+    var searchTerm = req.body.search_term;
+    var searchType = req.body.search_type;
+
+    searchTerm = searchTerm.toLowerCase();
+    searchType = searchType.toLowerCase();
+
+    if (searchType === "recepie") {
+        Recepie.find({}, function(err, recepies) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                if (recepies.length == 0) {
+                    res.render("search", {recepies: []});
+                }
+                else {
+                    var recepiesList = [];
+                    var i = 0;
+                    var j = 0;
+                    var temp = [];
+                    for (i = 0; i < recepies.length; i++) {
+                        temp = recepies[i].name.split(" ");
+                        for (j = 0; j < temp.length; j++) {
+                            if (searchTerm === temp[j]){
+                                recepiesList.push(recepies[i]);
+                                break;
+                            }
+                        }
+                        
+                    } 
+                    res.render("search", {recepies: recepiesList, userId: userId, search_term: searchTerm});
+                }
+            }
+        });
+    }
+    else {
+        Recepie.find({}, function(err, recepies) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                var i = 0;
+                var j = 0;
+                var recepiesList = [];
+                for (i = 0; i < recepies.length; i++) {
+                    for (j = 0; j < 4; j++) {
+                        console.log(searchTerm, recepies[i].ingredients[j]);
+                        if (searchTerm === recepies[i].ingredients[j]) {
+
+                            recepiesList.push(recepies[i]);
+                            break;
+                        }
+                    } 
+                }
+                console.log(recepies);
+                res.render("search", {recepies: recepiesList, userId: userId, search_term: searchTerm});
+            }
+        })
+    }
+
+    console.log(userId, searchTerm, searchType);
+});
+
+
+
 
 // ===========================================================
 
